18b0aea9a49f5e26f0d62648d29e5f3d1ee868b1
khana <khana@QWERTY>
2022-03-15 17:43:41 +0530

    Add project files.

---
+++ GridViewBasic/Gridview.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Gridview.aspx.cs" Inherits="GridViewBasic.Gridview" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:GridView runat="server" ID="gdview"></asp:GridView>
+        <asp:Button runat="server" Text="Show na bich" ID="btnshow" OnClick="btnshow_Click" />
+        <br />
+        <asp:TextBox runat="server" ID="txtname" placeholder="Student name"></asp:TextBox>
+        <br />
+        <asp:TextBox runat="server" ID="txtrollno" placeholder="Student Rollno"></asp:TextBox>
+        <br />
+        <asp:Button runat="server" Text="here🤌, Add some comedy(student)" ID="btnadd" OnClick="btnadd_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ GridViewBasic/Gridview.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GridViewBasic
+{
+    public partial class Gridview : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void retrivedata()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["str"].ToString());
+            SqlCommand cmd = new SqlCommand("Select * From SYCS", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            gdview.DataSource = dt;
+            gdview.DataBind();
+
+        }
+        protected void btnshow_Click(object sender, EventArgs e)
+        {
+            retrivedata();
+        }
+
+        protected void btnadd_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["str"].ToString());
+            SqlCommand cmd = new SqlCommand("insert into sycs values('"+txtname.Text+"','"+txtrollno.Text+"')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            txtname.Text =string.Empty;
+            txtrollno.Text = string.Empty;
+            retrivedata();
+
+        }
+    }
+}
